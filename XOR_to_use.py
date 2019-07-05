67d2f64c1c47d85fb1afdc24328b11cda85ffb1b
DianaChj <50831927+DianaChj@users.noreply.github.com>
2019-07-05 17:54:11 +0300

    Update XOR_to_use.py

--- XOR_to_use.py
+++ XOR_to_use.py
@@ -16,14 +16,15 @@ output_data = np.array([[0.], [1.], [1.], [0.]], dtype='uint8')
 n_input = tf.placeholder(tf.float32, shape=[None, 2], name="n_input")
 n_output = tf.placeholder(tf.float32, shape=[None, 1], name="n_output")
 
-hidden_nodes = 3
+hidden_nodes = 2
 
-b_hidden = tf.Variable(tf.random_normal([hidden_nodes]), name="hidden_bias")
+b_hidden = tf.Variable(tf.zeros([hidden_nodes]), name="hidden_bias")
+b2 = tf.Variable(tf.zeros([1]), name="Biases2")
 W_hidden = tf.Variable(tf.random_normal([2, hidden_nodes]), name="hidden_weights")
 hidden = tf.sigmoid(tf.matmul(n_input, W_hidden) + b_hidden)
 
 W_output = tf.Variable(tf.random_normal([hidden_nodes, 1]), name="output_weights")
-output = tf.sigmoid(tf.matmul(hidden, W_output))
+output = tf.sigmoid(tf.matmul(hidden, W_output) + b2)
 
 # Add ops to restore all the variables.
 saver = tf.train.Saver()
@@ -35,7 +36,7 @@ def XOR_func(x):
     with sess1.as_default() as sess:
         res = []
         for i in range(len(x)):
-            res.append(int(np.round(sess.run(output, feed_dict={n_input: np.array([x[i]])}))))
+            res.append(int(np.round(sess.run(output, feed_dict={n_input: np.array([x[i]])}))[0][0]))
         return res
 
 
